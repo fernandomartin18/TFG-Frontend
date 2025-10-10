185a6286900cfe744d942619fd13cf3657d7bff4
Fer <fernandomm1840@gmail.com>
2025-10-10 19:55:26 +0200

    Primer commit

---
+++ src/styles/themes.js
@@ -0,0 +1,22 @@
+const themes = {
+  light: {
+    background: '#f6f8fa',
+    text: '#222',
+    navbar: '#fafadaff',
+    navbarText: '#222',
+    navbarShadow: '0 2px 16px rgba(0,0,0,0.08)',
+    gradientBg: 'radial-gradient(ellipse 180% 180% at 50% -10%, #a29ff6c6, #ffffffff 50%)',
+    primary: '#06b9a7ff',
+  },
+  dark: {
+    background: '#181a1b',
+    text: '#f6f8fa',
+    navbar: '#323232ff',
+    navbarText: '#f6f8fa',
+    navbarShadow: '0 2px 16px rgba(54, 54, 54, 0.38)',
+    gradientBg: 'radial-gradient(ellipse 180% 150% at 50% -10%, #18013eff, #000000ff 50%)',
+    primary: '#40e0d0',
+  },
+}
+
+export default themes
