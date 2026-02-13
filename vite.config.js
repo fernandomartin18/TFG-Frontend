377bfe4c6dfd5bded4429b4d2674e3349018b4df
Fer <fernandomm1840@gmail.com>
2026-02-13 19:43:07 +0100

    (test): configurados tests y cobertura con vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js', // Crearemos este archivo ahora
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'lcov'], // 'lcov' es vital para Sonar
+    },
+  },
 })
